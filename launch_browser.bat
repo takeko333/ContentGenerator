9bba27920af47e4634ae3215fa9e451c60f72eca
takeko333 <takemaru180208@gmail.com>
2026-03-28 16:21:22 +0900

    260328

--- launch_browser.bat
+++ launch_browser.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\Users\takem\Workspace\Programming\Unofficial-API\config\chrome_profile"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\Users\takem\Workspace\Programming\ContentGenerator\config\chrome_profile"
