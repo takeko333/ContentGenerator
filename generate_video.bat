be6ea13d0b46cdbe5dbdf87c854c0ffd29434450
takeko333 <takemaru180208@gmail.com>
2026-05-02 13:09:09 +0900

    260502

--- generate_video.bat
+++ generate_video.bat
@@ -1,4 +1,4 @@
 @echo off
 set PYTHONPATH=%cd%\src
 python src/scripts/generate_video.py
-python src/scripts/delete_files_after_generating_video.py
+python src/scripts/delete_files_after_video_generation.py
